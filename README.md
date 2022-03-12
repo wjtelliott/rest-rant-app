7d3c350d0c9136e818a1d6785d467e3b5e94f221
wjtelliott <wjtelliott@gmail.com>
2022-03-12 08:58:58 -0600

    Changed some comments. Updated the readme

--- README.md
+++ README.md
@@ -35,10 +35,13 @@ We will include the following in our mock places data:
 | GET | `/place` | Places index page |
 | POST | `/place` | Create new place |
 | GET | `/places/new` | Form page for creating a new place |
-| GET | `/places/:id` | Details about particular place |
+| GET | `/places/:id *` | Details about particular place |
+| GET | `/places?index=id` | Details about particular place |
 | PUT | `/places/:id` | Update a particular place |
 | GET | `/places/:id/edit` | Form page for editing an existing place |
 | DELETE | `/places/:id` | Delete a particular place |
 | POST | `/places/:id/rant` | Create a rant (comment) about a particular place |
 | DELETE | `/places/:id/rant/:rantId` | Delete a rant (comment) about a particular place |
 | GET | `*` | 404 Page |
+
+*Obsolete
--- controllers/places.js
+++ controllers/places.js
@@ -2,17 +2,15 @@ const router = require('express').Router();
 
 const db = require('../models/mockPlacesData');
 
-// TODO: Create new 404 handler
+// TODO: Create new function for 404 handling
 router.get('/', (req, res) => {
     //* I know people won't like this init in the IF
-    if ((index = req.query?.index) == null) {
-        res.status(206).render('places/index', { data: db });
-    } else {
+    if ((index = req.query?.index) == null) res.status(206).render('places/index', { data: db });
+    else {
         if (isNaN(Number(index))) res.status(404).render('error404');
         else {
             let uid = -1;
-            for (let i = 0; i < db.length; i++)
-                if (Number(db[i].uid) === Number(index)) { uid = db.indexOf(db[i]); break; }
+            for (let i = 0; i < db.length; i++) if (Number(db[i].uid) === Number(index)) { uid = db.indexOf(db[i]); break; }
             uid !== -1 ? res.status(206).render('places/placeDetails', { data: db[uid] }) : res.status(404).render('error404');
         }
     }
