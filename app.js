97b6533c4135b7fec77b8f145996f5e8b9261744
wjtelliott <wjtelliott@gmail.com>
2022-03-14 21:43:55 -0500

    Updated some comments

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 const methodOverride = require('method-override');
 const logEvent = require('./assets/logEvents');
 
-//!app.use(express.static(path.join(__dirname, 'public')));
+////app.use(express.static(path.join(__dirname, 'public')));
 
 // Middleware
 app.use(express.static('public'));
